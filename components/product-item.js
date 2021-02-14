6fd67327fa1b125050762a7a899078400078d2f7
IRebmann92 <60708439+IRebmann92@users.noreply.github.com>
2021-02-13 21:41:35 -0800

    commiting

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,128 @@
 // product-item.js
 
+
 class ProductItem extends HTMLElement {
-  // TODO
+
+  render() {
+    const incart = this.getAttribute('incart');
+    const imgUrl = this.getAttribute('img');
+    const titleText = this.getAttribute('title');
+    const id = this.getAttribute('id');
+    const priceText = this.getAttribute('price');
+    
+    
+
+    // start inner
+    this.innerHTML = `
+ 
+ 
+    <style>
+  
+    .price {
+       color: white;
+       font-size: 1.9em;
+       font-weight: bold;
+       margin: 0;
+    }
+  
+    .product {
+       align-items: center;
+       background-color: black;
+       border-radius: 7px;
+       display: grid;
+       grid-template-areas:
+        'image'
+        'title'
+        'price'
+        'add';
+ 
+       grid-template-rows: 67% 11% 11% 11%;
+       height: 650px;
+       filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+       margin: 0 30px 30px 0;
+       padding: 10px 20px;
+       width: 250px;
+ 
+    }
+ 
+    .product > button {
+       background-color: green;
+       border: none;
+       border-radius: 5px;
+       color: white;
+       justify-self: center;
+       max-height: 40px;
+       padding: 8px 20px;
+       transition: 0.1s ease all;
+    }
+   
+    .product > button:hover {
+       background-color: cyan;
+       cursor: pointer;
+       transition: 0.1s ease all;
+    }
+ 
+    .product > img {
+       align-self: center;
+       justify-self: center;
+       width: 100%;
+    }
+ 
+    .title {
+       font-size: 1.4em;
+       margin: 0;
+       overflow: hidden;
+       text-overflow: ellipsis;
+       white-space: nowrap;
+    }
+
+    .title:hover {
+      font-size: 1.1em;
+      margin: 0;
+      white-space: wrap;
+      overflow: auto;
+      text-overflow: unset;
+    }
+ 
+    </style>
+ 
+    <li class="product">
+ 
+      <img src="${imgUrl}" alt="${titleText}" width=200>
+ 
+      <p class="title">${titleText}</p>
+ 
+      <p class="price">${priceText}</p>
+  
+      ${incart == "false" || incart == "undefined" ?
+
+      `<button onclick=remove_from_list('${id}')>Remove from Cart</button>`:
+        `<button onclick=add_to_list('${id}')>Add to Cart</button>` 
+
+        
+      }
+    </li>
+
+    `
+    // end inner
+  }
+  // end render
+   
+  connectedCallback() { 
+    if (!this.rendered) {
+      this.render();
+      this.rendered = true;
+    }
+  }
+
+  static get observedAttributes() { 
+    return ['img', 'title', 'price', 'incart'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.render();
+  }
+   
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,73 @@
 // Script.js
 
+let list = JSON.parse(localStorage.getItem("list"));
+let counter = document.querySelector("#cart-count");
+if (!list) {
+  list = {};
+}
+// getting type err with query selector fixed but still remove from cart shows
+function count_list() {
+  let count = 0;
+  for (prop in list) {
+    if (list[prop] == true) {
+      count++;
+    }
+  }
+  counter.textContent = count;
+}
+
+count_list();
+// pull list on browser start
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+// puling list from url then local storing 
+  if (!localStorage.getItem("products")) {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => {
+        localStorage.setItem("products", JSON.stringify(data));
+        create_cards(data);
+      }
+      );
+  }
+  else {
+    let data = localStorage.getItem("products");
+    data = JSON.parse(data);
+    create_cards(data);
+  }
 });
+
+
+// create cards 
+function create_cards(data) {
+  let list = document.querySelector("#product-list");
+  for (let i in data) {
+    let prodItem = document.createElement("product-item");
+    let prod = data[i];
+    prodItem.setAttribute("img", prod.image);
+    prodItem.setAttribute("title", prod.title);
+    prodItem.setAttribute("price", "$" + prod.price);
+    prodItem.setAttribute("id", "prod" + prod.id);
+    prodItem.setAttribute("in_list", list["prod" + prod.id]);
+    list.appendChild(prodItem);
+  }
+}
+
+
+
+function add_to_list(id) {
+  list[id] = true;
+  localStorage.setItem("list", JSON.stringify(list));
+  let card = document.querySelector("#" + id);
+  card.setAttribute("in_list", "true");
+  count_list();
+}
+
+
+
+function remove_from_list(id) {
+  list[id] = false;
+  localStorage.setItem("list", JSON.stringify(list));
+  let card = document.querySelector("#" + id);
+  card.setAttribute("in_list", "false");
+  count_list();
+}
